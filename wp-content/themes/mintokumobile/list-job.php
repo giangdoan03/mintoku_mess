b23095b792a4e6c2dfb0253b83c3d541b90ab2a6
Giang Doan <dvg8.vcc@gmail.com>
2024-09-19 11:13:12 +0700

    update

--- wp-content/themes/mintokumobile/list-job.php
+++ wp-content/themes/mintokumobile/list-job.php
@@ -72,7 +72,20 @@ $query = new WP_Query($args);
             <div class="content_list_job_filter">
                 <ul>
                     <?php while ($query->have_posts()) : $query->the_post(); ?>
-                        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                        <li>
+                            <?php
+                            // Get the featured image URL
+                            $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'medium');
+                            // Use a placeholder if no featured image is set
+                            if (!$thumbnail_url) {
+                                $thumbnail_url = 'https://placehold.co/600x400';
+                            }
+                            ?>
+                            <a href="<?php the_permalink(); ?>">
+                                <img src="<?php echo esc_url($thumbnail_url); ?>" alt="<?php the_title_attribute(); ?>" style="max-width:100%; height:auto;">
+                                <?php the_title(); ?>
+                            </a>
+                        </li>
                     <?php endwhile; ?>
                 </ul>
             </div>
@@ -82,7 +95,6 @@ $query = new WP_Query($args);
     </div>
 
     <script>
-
         async function fetchTranslations() {
             try {
                 const response = await fetch('<?php echo get_template_directory_uri(); ?>/js/translations.json');
@@ -111,7 +123,7 @@ $query = new WP_Query($args);
         jQuery(document).ready(function ($) {
             const language = '<?php echo ICL_LANGUAGE_CODE; ?>'; // PHP variable
             setLanguage(language);
-        })
+        });
     </script>
 <?php
 wp_reset_postdata();
